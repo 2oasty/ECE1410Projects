09baf83515330972788322fc51ee592e4414a5e0
Jeremy Chin <parkourmc3@gmail.com>
2022-01-24 01:56:52 -0700

    Added HelloWorld program and WindowsRun screenshot

---
+++ HelloWorld/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+/******************************************************************************
+* Function Title: main
+* 
+* Summary: Prints "Hello World"
+* 
+* Inputs: None
+* Outputs: None
+* 
+* Linux Compile Instructions: g++ Source.cpp -o HelloWorld
+*******************************************************************************
+* Pseudocode:
+* 
+* Begin
+*	Print "Hello World"
+* End
+******************************************************************************/
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+
+	return 0;
+}
