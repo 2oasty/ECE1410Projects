1ae9e397e63c275396766f6f0aea76cd4488be2f
Jeremy Chin <parkourmc3@gmail.com>
2022-02-06 21:06:26 -0700

    Created Conway'sGameOfLife and openFile function

---
+++ Conway'sGameOfLife/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <vector>
+
+void openFile(std::string const& input, std::string const& output, std::ifstream& fin, std::ofstream& fout);
+
+/******************************************************************************
+* Function Title: main
+* 
+* Summary:
+* 
+* Inputs:
+* Outputs:
+*******************************************************************************
+* Pseudocode:
+* 
+* Begin
+* End
+******************************************************************************/
+int main(int argc, char* argv[]) 
+{
+	std::string inputFile, outputFile;
+	std::ifstream fin;
+	std::ofstream fout;
+
+	inputFile = argv[1];
+	outputFile = argv[2];
+	
+	openFile(inputFile, outputFile, fin, fout);
+
+	return 0;
+}
+
+void openFile(std::string const& input, std::string const& output, std::ifstream& fin, std::ofstream& fout) 
+{
+	//Opens input file for reading
+	fin.open(input);
+	if (!fin.good()) 
+	{
+		std::cout << "Input file doesn't exist, exiting..." << std::endl;
+		exit(1);
+	}
+
+	fout.open(output);
+}
