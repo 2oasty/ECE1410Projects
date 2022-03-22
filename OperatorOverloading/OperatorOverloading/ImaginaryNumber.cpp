78343e8bdb15d485cd872320359cacc17d07e63f
Jeremy Chin <parkourmc3@gmail.com>
2022-03-22 01:54:47 -0600

    Got tests to run. Implementation incorrect

--- OperatorOverloading/OperatorOverloading/ImaginaryNumber.cpp
+++ OperatorOverloading/OperatorOverloading/ImaginaryNumber.cpp
@@ -1,4 +1,7 @@
-#include "ImaginaryNumber.hpp".
+#include <iostream>
+#include <ostream>
+#include "ImaginaryNumber.hpp"
+
 
 ImaginaryNumber::ImaginaryNumber() : m_real(0), m_imaginary(0)
 {
@@ -9,4 +12,38 @@ ImaginaryNumber::ImaginaryNumber(double real, double imaginary) :
 	m_real(real), m_imaginary(imaginary)
 {
 
+}
+
+ImaginaryNumber& ImaginaryNumber::operator+=(ImaginaryNumber const& rhs) 
+{
+    this->m_real += rhs.m_real;
+    return *this;
+}
+
+ImaginaryNumber& ImaginaryNumber::operator-=(ImaginaryNumber const& rhs)
+{
+    this->m_real -= rhs.m_real;
+    return *this;
+}
+
+ImaginaryNumber& ImaginaryNumber::operator*=(ImaginaryNumber const& rhs)
+{
+    this->m_real *= rhs.m_real;
+    return *this;
+}
+
+ImaginaryNumber& ImaginaryNumber::operator/=(ImaginaryNumber const& rhs)
+{
+    this->m_real /= rhs.m_real;
+    return *this;
+}
+
+double ImaginaryNumber::getReal()
+{
+    return 0;
+}
+
+double ImaginaryNumber::getImaginary()
+{
+    return 0;
 }
--- OperatorOverloading/OperatorOverloading/ImaginaryNumber.hpp
+++ OperatorOverloading/OperatorOverloading/ImaginaryNumber.hpp
@@ -14,39 +14,107 @@ public:
 	//Copy Constructor (For Linux)
 
 	//Overload <<
+	friend std::ostream& operator<<(std::ostream& os, ImaginaryNumber const& num)
+	{
+		if (num.m_real == 0 && num.m_imaginary == 0)
+		{
+			os << "0";
+		}
+		else if (num.m_real == 0)
+		{
+			os << num.m_imaginary << "j";
+		}
+		else if (num.m_imaginary == 0)
+		{
+			os << num.m_real;
+		}
+		else
+		{
+			os << num.m_real << " + " << num.m_imaginary << "j";
+		}
+
+		return os;
+	}
 
 	//Overload +=
+	ImaginaryNumber& operator+=(ImaginaryNumber const& rhs);
 
 	//Overload +
+	friend ImaginaryNumber operator+(ImaginaryNumber lhs, ImaginaryNumber const& rhs)
+	{
+		lhs += rhs;
+		return lhs;
+	}
 
 	//Overload -=
+	ImaginaryNumber& operator-=(ImaginaryNumber const& rhs);
+
 
 	//Overload -
+	friend ImaginaryNumber operator-(ImaginaryNumber lhs, ImaginaryNumber const& rhs)
+	{
+		lhs -= rhs;
+		return lhs;
+	}
 
 	//Overload *=
+	ImaginaryNumber& operator*=(ImaginaryNumber const& rhs);
 
 	//Overload *
+	friend ImaginaryNumber operator*(ImaginaryNumber lhs, ImaginaryNumber const& rhs)
+	{
+		lhs *= rhs;
+		return lhs;
+	}
 
 	//Overload /=
+	ImaginaryNumber& operator/=(ImaginaryNumber const& rhs);
+
 
 	//Overload /
+	friend ImaginaryNumber operator/(ImaginaryNumber lhs, ImaginaryNumber const& rhs)
+	{
+		lhs /= rhs;
+		return lhs;
+	}
 
 	//Overload =
 
 	//Overload <
+	friend bool operator< (ImaginaryNumber const& lhs, ImaginaryNumber const& rhs)
+	{
+		return lhs.m_real == rhs.m_real;
+	}
 
 	//Overload <=
+	friend bool operator<=(ImaginaryNumber const& lhs, ImaginaryNumber const& rhs)
+	{ 
+		return lhs.m_real == rhs.m_real;
+	}
 
 	//Overload >
+	friend bool operator> (ImaginaryNumber const& lhs, ImaginaryNumber const& rhs)
+	{ 
+		return lhs.m_real == rhs.m_real;
+	}
 
 	//Overload >=
+	friend bool operator>=(ImaginaryNumber const& lhs, ImaginaryNumber const& rhs)
+	{ 
+		return lhs.m_real == rhs.m_real;
+	}
 
 	//Overload ==
+	friend bool operator==(ImaginaryNumber const& lhs, ImaginaryNumber const& rhs)
+	{
+		return lhs.m_real == rhs.m_real;
+	}
 
 	//Gets real portion of imaginary number
+	double getReal();
 
 	//Gets imaginary portion of imaginary number
-
+	double getImaginary();
 
 private:
 	double m_real;
