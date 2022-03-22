c1ac6c8443d1e3426adb35e6f11339ec52e470bf
Jeremy Chin <parkourmc3@gmail.com>
2022-03-22 00:05:31 -0600

    Started ImaginaryNumbers

---
+++ OperatorOverloading/OperatorOverloading/ImaginaryNumber.cpp
@@ -0,0 +1,12 @@
+#include "ImaginaryNumber.hpp".
+
+ImaginaryNumber::ImaginaryNumber() : m_real(0), m_imaginary(0)
+{
+
+}
+
+ImaginaryNumber::ImaginaryNumber(double real, double imaginary) : 
+	m_real(real), m_imaginary(imaginary)
+{
+
+}
---
+++ OperatorOverloading/OperatorOverloading/ImaginaryNumber.hpp
@@ -0,0 +1,57 @@
+#ifndef IMAGINARYNUMBER_HPP
+#define IMAGINARYNUMBER_HPP
+
+class ImaginaryNumber
+{
+public:
+	
+	//Default Constructor
+	ImaginaryNumber();
+
+	//Parameter Constructor
+	ImaginaryNumber(double real, double imaginary);
+
+	//Copy Constructor (For Linux)
+
+	//Overload <<
+
+	//Overload +=
+
+	//Overload +
+
+	//Overload -=
+
+	//Overload -
+
+	//Overload *=
+
+	//Overload *
+
+	//Overload /=
+
+	//Overload /
+
+	//Overload =
+
+	//Overload <
+
+	//Overload <=
+
+	//Overload >
+
+	//Overload >=
+
+	//Overload ==
+
+	//Gets real portion of imaginary number
+
+	//Gets imaginary portion of imaginary number
+
+
+private:
+	double m_real;
+	double m_imaginary;
+};
+
+
+#endif
