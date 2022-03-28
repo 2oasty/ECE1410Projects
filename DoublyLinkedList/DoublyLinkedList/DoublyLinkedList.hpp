8ed71b6e95b3b1f6845bc393381c0203400ad3eb
Jeremy Chin <parkourmc3@gmail.com>
2022-03-27 21:06:53 -0600

    Started DoublyLinkedList

---
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.hpp
@@ -0,0 +1,53 @@
+#ifndef DOUBLY_LINKED_LIST_HPP
+#define DOUBLY_LINKED_LIST_HPP
+
+#include <memory>
+#include <stdexcept>
+
+template <typename T>
+class DoublyLinkedList
+{
+public:
+	// grader
+	// DoublyLinkedList<T>();
+	DoublyLinkedList<T>()
+	{
+
+	}
+
+	//void create(T value)
+	void create(T value)
+	{
+		
+	}
+
+	// T retreive(T value)
+	T retreive(T value)
+	{
+
+	}
+
+	//void update(T oldValue, T newValue)
+	void update(T oldValue, T newValue)
+	{
+
+	}
+
+	// void remove(T value)
+	void remove(T value)
+	{
+
+	}
+
+	//size_t size()
+	size_t size()
+	{
+
+	}
+
+private:
+
+
+};
+
+#endif
