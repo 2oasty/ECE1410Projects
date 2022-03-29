40fd76147f200afc6cb90ba5004dff6b5344bf1f
Jeremy Chin <parkourmc3@gmail.com>
2022-03-28 19:15:22 -0600

    Altered CMakeLists to run on Windows. Added create function

--- DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.hpp
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.hpp
@@ -8,23 +8,53 @@ template <typename T>
 class DoublyLinkedList
 {
 public:
-	// grader
+	
+	struct Node {
+		T data;
+		std::shared_ptr<Node> next;
+		std::shared_ptr<Node> prev;
+	};
+
 	// DoublyLinkedList<T>();
-	DoublyLinkedList<T>()
+	DoublyLinkedList<T>() : head(nullptr), tail(nullptr)
 	{
 
+	}
+
+	//createNode
+	std::shared_ptr<Node> createNode(T value)
+	{
+		//!!!Need to allocate as smart pointer!!!
+		std::shared_ptr<Node> newNode(new Node);
+		newNode->data = value;
+		newNode->prev = nullptr;
+		newNode->next = nullptr;
+		return newNode;
 	}
 
 	//void create(T value)
 	void create(T value)
 	{
-		
+		std::shared_ptr<Node> newNode = createNode(value);
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+			return;
+		}
+
+		head->prev = newNode;
+		newNode->next = head;
+		tail = head;
+		head = newNode;
 	}
 
 	// T retreive(T value)
 	T retreive(T value)
 	{
-
+		T temp = value;
+		return temp;
 	}
 
 	//void update(T oldValue, T newValue)
@@ -42,11 +72,12 @@ public:
 	//size_t size()
 	size_t size()
 	{
-
+		return 5;
 	}
 
 private:
-
+	std::shared_ptr<Node> head;
+	std::shared_ptr<Node> tail;
 
 };
 
