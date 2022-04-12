a2ac1a97b6a393630a48fef485b4affe5f861a56
Jeremy Chin <parkourmc3@gmail.com>
2022-04-11 19:58:24 -0600

    Added bubble sort

--- Sorting/Sorting/SortTestOwn.cpp
+++ Sorting/Sorting/SortTestOwn.cpp
@@ -1,5 +1,6 @@
 
 #include <gtest/gtest.h>
+#include <algorithm>
 
 #include "BigVector.hpp"
 
@@ -9,6 +10,21 @@
 
 TEST(StudentWrittenTests, BubbleSort)
 {
+	std::vector<int> numbersOwn1 = { 0, 0, 0, 0 };
+	std::vector<int> numbersOwn2 = { 5, -20, 8, 1000, 43, -2 };
+	std::vector<int> numbersOwn3 = { 13 };
+
+	auto ans1 = bubble::sort(numbersOwn1);
+	std::sort(numbersOwn1.begin(), numbersOwn1.end());
+	EXPECT_EQ(ans1, numbersOwn1);
+
+	auto ans2 = bubble::sort(numbersOwn2);
+	std::sort(numbersOwn2.begin(), numbersOwn2.end());
+	EXPECT_EQ(ans2, numbersOwn2);
+
+	auto ans3 = bubble::sort(numbersOwn3);
+	std::sort(numbersOwn3.begin(), numbersOwn3.end());
+	EXPECT_EQ(ans3, numbersOwn3);
 }
 
 TEST(StudentWrittenTests, SelectionSort)
---
+++ Sorting/Sorting/Sorting.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "BubbleSort.hpp"
+#include "SelectionSort.hpp"
+#include "MergeSort.hpp"
+
+//Bubble Sort
+std::vector<int> bubble::sort(std::vector<int> vectorToSort)
+{
+	for (int i = 0; i < vectorToSort.size() - 1; i++)
+	{
+		for (int j = 0; j < vectorToSort.size() - 1; j++)
+		{
+			if (vectorToSort[j] > vectorToSort[j + 1])
+			{
+				std::swap(vectorToSort[j], vectorToSort[j + 1]);
+			}
+		}
+	}
+
+	return vectorToSort;
+}
+
+//Selection Sort
+std::vector<int> selection::sort(std::vector<int> vectorToSort)
+{
+	return vectorToSort;
+}
+
+//Merge Sort
+std::vector<int> merge::sort(std::vector<int> vectorToSort)
+{
+	return vectorToSort;
+}
