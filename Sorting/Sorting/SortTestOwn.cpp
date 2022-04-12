f8fc32b0117db0183d19b5d6b60becaf664647ad
Jeremy Chin <parkourmc3@gmail.com>
2022-04-11 19:16:08 -0600

    Started sorting assignment

---
+++ Sorting/Sorting/SortTestOwn.cpp
@@ -0,0 +1,20 @@
+
+#include <gtest/gtest.h>
+
+#include "BigVector.hpp"
+
+#include "BubbleSort.hpp"
+#include "SelectionSort.hpp"
+#include "MergeSort.hpp"
+
+TEST(StudentWrittenTests, BubbleSort)
+{
+}
+
+TEST(StudentWrittenTests, SelectionSort)
+{
+}
+
+TEST(StudentWrittenTests, MergeSort)
+{
+}
