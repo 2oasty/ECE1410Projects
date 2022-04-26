553855bc3a458365856b079a44b0fe6059f67809
Jeremy Chin <parkourmc3@gmail.com>
2022-04-25 19:14:58 -0600

    Added add and at functions. Passes own tests.

--- FinalProject/FinalProject/BST.hpp
+++ FinalProject/FinalProject/BST.hpp
@@ -1,6 +1,9 @@
 #ifndef BST_HPP
 #define BST_HPP
 
+#include <memory>
+#include <stdexcept>
+
 enum class Direction
 {
 	LEFT,
@@ -12,35 +15,126 @@ class Bst
 {
 public:
 
+	struct Node {
+		Key key{};
+		Value value{};
+		std::shared_ptr<Node> left;
+		std::shared_ptr<Node> right;
+	};
 
+	//Default constructor
+	Bst() : head(nullptr)
+	{
 
-	Bst();
+	}
 
 	//Copy constructor
-	Bst(Bst const& bst);
+	Bst(Bst const& bst)
+	{
 
-	//Add
-	void add(Key const& key, Value const& value);
+	}
+
+	//createNode
+	std::shared_ptr<Node> createNode(Key const& key, Value const& value)
+	{
+		std::shared_ptr<Node> newNode = std::make_shared<Node>();
+		newNode->key = key;
+		newNode->value = value;
+		newNode->left = nullptr;
+		newNode->right = nullptr;
+		return newNode;
+	}
+
+	//Add (logn or n)
+	void add(Key const& key, Value const& value)
+	{
+		addRec(head, key, value);
+	}
+
+	//Recursive add
+	void addRec(std::shared_ptr<Node> &temp, Key const& key, Value const& value)
+	{
+		//Nothing in BST
+		if (temp == nullptr)
+		{
+			temp = createNode(key, value);
+			return;
+		}
+
+		//Key already exists
+		if (key == temp->key)
+		{
+			return;
+		}
+
+		//Nodes exist in BST
+		if (key < temp->key)
+		{
+			addRec(temp->left, key, value);
+		}
+		else if (key > temp->key)
+		{
+			addRec(temp->right, key, value);
+		}
+	}
 
 	//Update
-	void update(Key const& key, Value const& value);
+	void update(Key const& key, Value const& value)
+	{
+
+	}
 	
 	//Remove
-	void remove(Key const& key);
+	void remove(Key const& key)
+	{
+
+	}
 
 	//At
-	Value& at(Key const& key);
+	Value& at(Key const& key)
+	{
+		std::shared_ptr<Node> temp = head;
+
+		while (temp != nullptr)
+		{
+			if (key == temp->key)
+			{
+				return temp->value;
+			}
+			else if (key < temp->key)
+			{
+				temp = temp->left;
+			}
+			else if (key > temp->key)
+			{
+				temp = temp->right;
+			}
+		}
+		
+		throw std::runtime_error("Key doesn't exist in BST");
+
+	}
 
 	//Operator[]
-	Value& operator[](Key const& key);
+	Value& operator[](Key const& key)
+	{
+		return at(key);
+	}
 
 	//Traverse
-	Value traverse(std::vector<Direction> const& path);
+	Value traverse(std::vector<Direction> const& path)
+	{
+		return at(key);
+	}
 
 	//Size
-	size_t size();
+	size_t size()
+	{
+		return 5;
+	}
 
 private:
+	std::shared_ptr<Node> head;
 };
 
 #endif
--- FinalProject/FinalProject/BSTTestOwn.cpp
+++ FinalProject/FinalProject/BSTTestOwn.cpp
@@ -5,4 +5,14 @@
 
 TEST(OwnWrittenTests, BSTOwn)
 {
+	Bst<int, int> bst;
+	bst.add(42, 1);
+	bst.add(24, -7);
+	bst.add(56, 0);
+	bst.add(13, 12);
+	bst.add(26, -10);
+	EXPECT_EQ(bst.at(24), -7);
+	EXPECT_EQ(bst.at(26), -10);
+	EXPECT_THROW(bst.at(3), std::runtime_error);
+
 }
